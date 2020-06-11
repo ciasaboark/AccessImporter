7559d7507454631d2a8696dbb7920019b9034ee6
Jonathan Nelson <ciasaboark@gmail.com>
2020-06-11 18:22:29 -0400

    updated watcher -adjusted sleep interval to 10 minutes, triggers a manual import attempt on each wake -removed use of static methods in Watcher -early error conditions now trigger a immediate exit to prevent extraneous error messages -archived files are given a postfix with a timestamp to guard against previously archived files blocking the file move

--- watcher.py
+++ watcher.py
@@ -12,14 +12,20 @@ import struct
 import pyodbc
 import configargparse
 import textwrap
+import traceback
 from pprint import pformat
+from datetime import datetime
+from pathlib import Path
 
 VERSION = "0.0.1"
+
+#The interval (in seconds) between sleep cycles. A manual check is performed on each wake
+SLEEP_INTERVAL = 1 * 60 * 10
 
 IMPORT_FILE_TYPES = [".accdb"]
 
 logger = None
-
+watcher = None
 
 class Watcher(Daemon):
     def __init__(self, pidfile, args, parser):
@@ -36,11 +42,19 @@ class Watcher(Daemon):
         logger.info("* Version {0}".format(VERSION))
         logger.info("**************************************")
 
-        # pp = pprint.PrettyPrinter(indent=4)
         logger.info("Running with config:")
         logger.info(pformat(self.args, indent=1, width=80, depth=None, compact=False))
 
         event_handler = Handler()
+
+        # #Convert the paths into the OS specific formatting.
+        # # This allows paths to be defined in the config file as C:/import
+        # # and be auto converted to C:\import
+        # for index, p in enumerate(args.watch):
+        #     converted_path = Path(p)
+        #     args.watch[index] = converted_path
+        # args.archive = Path(args.archive)
+        # args.database = Path(args.database)
 
         #Quick test to see if we have the MS Access driver installed
         #Get a list of all installed Access DB drivers
@@ -48,7 +62,7 @@ class Watcher(Daemon):
         if len(drivers) == 0:
             logger.error('Unable to find any Access database drivers installed')
             log_access_driver_error()
-            raise Exception
+            sys.exit(1)
         elif 'Microsoft Access Driver (*.mdb, *.accdb)' not in drivers:
             #We have at least one Access driver installed, but it is the older version
             logger.error('Unable to find the updated MS Access database driver')
@@ -96,14 +110,21 @@ class Watcher(Daemon):
                 logger.error("Choose a different archive directory or remove this directory from the list of watched directories")
                 sys.exit(1)
 
-        #Everything looks OK.  Fire up the observer and begin the main loop
+        self.manual_import()
+
+        #  Fire up the observer and begin the main loop
         self.observer.start()
         logger.info("Watchdog running. Monitoring new files in paths {0}".format(pformat(self.watched_directories, indent=1, width=80, depth=None, compact=False)))
         logger.info("Completed imports will be moved into '{0}'".format(self.args.archive))  
 
         try:
             while True:
-                time.sleep(5)
+                #sleep for 10 minute intervals before triggering a manual check for any database
+                # files that need to be imported.  The on_created() callback will still fire during
+                # this interval.  The manual check is needed to handle importing files that failed
+                # during the on_created() callback (for example if the database is locked)
+                time.sleep(SLEEP_INTERVAL)
+                self.manual_import()
 
         except:
             self.observer.stop()
@@ -111,24 +132,41 @@ class Watcher(Daemon):
 
         self.observer.join()
 
-    @staticmethod
-    def check_file(path: str):
+    def manual_import(self):
+        #Everything looks OK.  Check for any pre-existing database files to import.  Since
+        # these already exist we won't get a on_created() callback in the Handler
+        logger.debug("Performing manual check for database files to import")
+        to_import = []
+        for d in self.args.watch:
+            for file in os.listdir(d):
+                full_path = os.path.join(d, file)
+                if os.path.isfile(full_path):
+                    to_import.append(full_path)
+
+        if len(to_import) == 0:
+            logger.debug("No files found to import")
+        else:
+            logger.debug("Found existing files in the watched directories.")
+            for f in to_import:
+                self.check_file(f)
+
+    def check_file(self, path: str):
         """
         Check the file path to see if it contains an Access database file.  This check is done
         strictly by the file extension
         """
+        logger.info("Checking file '{0}'".format(path))
         isDb = list(filter(path.endswith, IMPORT_FILE_TYPES))
         if isDb:
             #begin import
             logger.debug("File {} looks like an Access database, beginning import".format(path))
-            Watcher.import_file(path)
+            self.import_file(path)
         else:
             logger.debug("File {} does not look like an Access database, skipping...".format(path))
     
 
-    @staticmethod
-    def import_file(file: str, archive_dir: str):
-        importer = Importer(file)
+    def import_file(self, file: str):
+        importer = Importer(self.args.database, file)
 
         #should the file be moved to the archive directory after the import?
         moveFile = False
@@ -138,19 +176,32 @@ class Watcher(Daemon):
         except ImportException as e:
             logger.warning("Unable to import data from file: {0}".format(e.message))
             logger.warning("This is a recoverable error. The import will be attempted again later")
-            logging.error(traceback.format_exc())
+            logging.exception(e, exc_info=True)
         except FileFormatException as e:
             logger.error("Unable to import data from file: {0}".format(e.message))
             logger.error("This is a non-recoverable error. The import will not be attempted again later.")
-            logging.error(traceback.format_exc())
+            moveFile = True
+            logging.exception(e, exc_info=True)
+        except pyodbc.Error as e:
+            logger.error("Unable to import data from file: {0}".format(str(e)))
+            logger.error("Since this was a database error this may be recoverable.  The import will be attempted again later")
+            logging.exception(e, exc_info=True)
         except Exception as e:
             logger.error("Unable to import data from file for an unknown reason")
             logger.error("This is a non-recoverable error. The import will not be attempted again later.")
-            logging.error(traceback.format_exc())
+            moveFile = True
+            logging.exception(e, exc_info=True)
         finally:
             if moveFile:
-                logger.debug("Moving file {0} to archive")
-                shutil.move(file, archive_dir)
+                postfix = datetime.today().strftime('%Y-%m-%d-%H-%M-%S-%f')
+
+                newName = "{0}.{1}".format(
+                    os.path.basename(file),
+                    postfix
+                )
+                newPath = os.path.join(self.args.archive, newName)
+                logger.debug("Archiving file as '{0}'.".format(newPath))
+                shutil.move(file, newPath)
         
             
 
@@ -163,7 +214,7 @@ class Handler(events.FileSystemEventHandler):
         if not event.is_directory:    
             src = event.src_path
             logger.debug("File created: '{0}'".format(src))
-            Watcher.check_file(event.src_path)
+            watcher.check_file(event.src_path)
         
 
 
@@ -200,26 +251,26 @@ if __name__ == '__main__':
              
          ''')
     )
-    p.add_argument('-c', '--config', help='<Required> The configuration file to use.', required=True, is_config_file=True)
-    p.add('-w', '--watch', required=True, help='<Required> The directory to watch for new database files. Use additional -w arguments to watch multiple directories.', action='append')
-    p.add('-a', '--archive', required=True, help='<Required> The archive directory to hold imported database files')
-    p.add('-d', '--database', required=True, help='<Required> The path of the Access database file to import into')
-    
+    p.add_argument('-c', '--config', help='<Required> The configuration file to use.', is_config_file=True)
+    p.add('-w', '--watch', required=True, help='<Required> The directory to watch for new database files. Use additional -w arguments to watch multiple directories. At least one of the watched directories must exist.', action='append')
+    p.add('-a', '--archive', required=True, help='<Required> The archive directory to hold imported database files. This directory must already exist.')
+    p.add('-d', '--database', required=True, help='<Required> The path of the Access database file to import into. This directory must already exist')
+    p.add('action', choices=['start', 'stop', 'restart'], nargs="?",  help='<Optional> Run the program as a daemon.  If omitted the program will run in the foreground.')
 
     args = p.parse_args()
 
-    daemon = Watcher('/tmp/returns-watcher.pid', args, p)
-    if len(sys.argv) == 2:
+    watcher = Watcher('/tmp/returns-watcher.pid', args, p)
+    if args.action is not None:
         log_to_file()
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
+        if 'start' == args.action:
+            watcher.start()
+        elif 'stop' == args.action:
+            watcher.stop()
+        elif 'restart' == args.action:
+            watcher.restart()
         else:
-            print("Unknown command: '{}'".format(sys.argv[1]))
-            print("usage: {} start|stop|restart".format(sys.argv[0]))
+            print("Unknown command: '{}'".format(args.action))
+            print("usage: {} start|stop|restart".format(args.action))
             sys.exit(1)
         sys.exit(0)
     else:
@@ -229,7 +280,7 @@ if __name__ == '__main__':
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
         logger.addHandler(ch)
-        daemon.run()
+        watcher.run()
         sys.exit(2)
 
 
